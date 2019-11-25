fbc59a0ad609c0e48aefb04ce0276997ace800ac
Nicole <nicoleotieno18@gmail.com>
2019-11-25 23:58:18 +0300

    establish connection before running tests

--- test/connection.js
+++ test/connection.js
@@ -1,12 +1,18 @@
 //this file contains all of the code for connecting to the database
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/nickydata");
+//Connect to the db before tests run
+before(function(done) {
+  //before tells mocha to wait till connection has beeen established before running tests
+  //Connect to mongodb
+  mongoose.connect("mongodb://localhost/nickydata");
 
-mongoose.connection
-  .once("open", function() {
-    console.log("Connection has been made...");
-  })
-  .on("error", function(error) {
-    console.log("Connetion error:", error);
-  });
+  mongoose.connection
+    .once("open", function() {
+      console.log("Connection has been made...");
+      done(); //telling mocha that the connection has been established
+    })
+    .on("error", function(error) {
+      console.log("Connetion error:", error);
+    });
+});
